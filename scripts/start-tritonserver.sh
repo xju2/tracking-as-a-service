d434e7faf38598e01c362935c727b999ada55adf
Xiangyang Ju <xju@lbl.gov>
2025-05-09 10:04:19 -0700

    standard startup

--- scripts/start-tritonserver.sh
+++ scripts/start-tritonserver.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-WORK_DIR="$( dirname "${BASH_SOURCE[0]}" )/../"
+OUTPUTFILE="node_id.txt"
+
+while getopts "o:" opt; do
+  case $opt in
+    o)
+      OUTPUTFILE=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+OUTPUTFILE="$(realpath $OUTPUTFILE)"
+
+
+WORK_DIR="$(dirname "${BASH_SOURCE[0]}" )/../"
 WORK_DIR=$(readlink -f $WORK_DIR)
 TRITON_MODELS="${WORK_DIR}/models"
 TRITON_IMAGE="docker.io/docexoty/tritonserver:latest"
 
 TRITON_JOBS_DIR="${WORK_DIR}/jobs"
-TRITON_LOGS=$TRITON_JOBS_DIR/$SLURM_JOB_ID
-mkdir -p $TRITON_LOGS
+mkdir -p $TRITON_JOBS_DIR
 
 TRITON_LOG_VERBOSE=false
 
 
 TRITON_LOG_VERBOSE_FLAGS=""
 TRITON_SEVER_NAME="${SLURMD_NODENAME}"
-echo "$SLURMD_NODENAME" > $TRITON_JOBS_DIR/node_id.txt
+echo "$SLURMD_NODENAME" > $OUTPUTFILE
 
 #Setup Triton flags
 if [ "$TRITON_LOG_VERBOSE" = true ]
@@ -25,10 +41,10 @@ fi
 #Start Triton
 echo "[slurm] starting $TRITON_SEVER_NAME"
 podman-hpc run -it --rm --gpu --shm-size=20GB -p 8002:8002 -p 8001:8001 -p 8000:8000 \
-    --volume="$TRITON_MODELS:/models" -w $TRITON_LOGS \
+    --volume="$TRITON_MODELS:/models" -w $WORK_DIR \
     -v $WORK_DIR:$WORK_DIR \
     $TRITON_IMAGE \
     tritonserver \
         --model-repository=/models \
         $TRITON_LOG_VERBOSE_FLAGS  2>&1 \
-        | tee $TRITON_LOGS/$TRITON_SEVER_NAME.log
+        | tee $TRITON_JOBS_DIR/$TRITON_SEVER_NAME.log
