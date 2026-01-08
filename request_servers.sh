ac03c837d5322ed5410d8eeea2b84de0b09b6f1f
Xiangyang Ju <xju@lbl.gov>
2026-01-08 11:46:42 -0800

    batch

--- request_servers.sh
+++ request_servers.sh
@@ -3,7 +3,6 @@
 #SBATCH --job-name=tritonserver
 #SBATCH -C "gpu&hbm80g"
 #SBATCH -N 1
-#SBATCH -G 1
 #SBATCH --time=48:00:00
 #SBATCH --output=tritonserver-%j.out
 #SBATCH --error=tritonserver-%j.err
