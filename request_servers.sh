b845ff48e1f25386bc8c8ebbe51b3391e1d845b7
Xiangyang Ju <xju@lbl.gov>
2026-01-08 11:15:54 -0800

    default to 1 GPU

--- request_servers.sh
+++ request_servers.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=tritonserver
 #SBATCH -C "gpu&hbm80g"
 #SBATCH -N 1
-#SBATCH -G 4
+#SBATCH -G 1
 #SBATCH --time=48:00:00
 #SBATCH --output=tritonserver-%j.out
 #SBATCH --error=tritonserver-%j.err
